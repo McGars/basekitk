e42d2373c17571160a6885179d51be7f8cc63066
Gars <kaminary3@gmail.com>
2017-08-03 17:13:12 +0300

    update version

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ It contains frequently used functions and ui features such as List, navigation,
 route between pages, loading etc.
 
 ```gradle
-compile 'com.github.msgars:basekitk:0.0.12'
+compile 'com.github.msgars:basekitk:0.0.13'
 ```
 
 See example for more details
--- app/src/main/java/com/mcgars/basekitkotlin/App.kt
+++ app/src/main/java/com/mcgars/basekitkotlin/App.kt
@@ -1,16 +1,23 @@
 package com.mcgars.basekitkotlin
 
 import android.app.Application
+import com.mcgars.basekitk.config.KitBuilder
+import com.mcgars.basekitk.config.KitConfig
+import com.mcgars.basekitk.config.KitConfiguration
 import com.mcgars.basekitk.tools.pagecontroller.PageController
 import com.mcgars.basekitkotlin.controller.HelloAc
 
 /**
  * Created by gars on 16.05.17.
  */
-class App : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        // need for [com.mcgars.basekitk.features.simple.SimpleActivity]
-        PageController.baseActivityController = HelloAc::class.java
-    }
+class App : Application(), KitConfiguration {
+
+    val kitConfig =
+        KitBuilder.getInstance()
+                // need for [com.mcgars.basekitk.features.simple.SimpleActivity]
+                .setActivityController(HelloAc::class.java)
+                .build()
+
+    override fun getConfiguration() = kitConfig
+
 }
